e56239c1e19fb51530727af1202f5e01f789888c
christian-anker-larsen <christian.anker.larsen@maersk.com>
2022-05-01 22:42:02 +0200

    added tests

--- ConsoleLommeregner/Calculator.cs
+++ ConsoleLommeregner/Calculator.cs
@@ -6,23 +6,23 @@ using System.Threading.Tasks;
 
 namespace ConsoleLommeregner
 {
-    internal class Calculator
+    public class Calculator
     {
-        internal double Add(double x, double y)
+        public double Add(double x, double y)
         {
             return x + y;
         }
-        internal double Subtract(double x, double y)
+        public double Subtract(double x, double y)
         {
             return x - y;
         }
 
-        internal double Divide(double x, double y)
+        public double Divide(double x, double y)
         {
             return (double)x / (double)y;
         }
 
-        internal double Multiply(double x, double y)
+        public double Multiply(double x, double y)
         {
             return x * y;
         }
